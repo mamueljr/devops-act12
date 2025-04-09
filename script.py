001811adad11bd03b9f45b887bf08894a7d7ee12
Emmanuel Rojas <mamueljr@gmail.com>
2025-04-08 18:15:55 -0600

    primer commit

---
+++ script.py
@@ -0,0 +1,4 @@
+print("🚀 Ejecutando script de prueba...")
+with open("resultados.txt", "w") as f:
+    f.write(f"Script ejecutado con éxito el: {datetime.datetime.now()}")
+print("✅ Archivo 'resultados.txt' generado!")
